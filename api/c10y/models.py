6825034db8d4a51b7f6c189a2fa62fa9cc615c7f
Carlton Duffett <carlton.duffett@gmail.com>
2023-08-05 14:26:22 -0700

    Add Constituent model

--- api/c10y/models.py
+++ api/c10y/models.py
@@ -1,6 +1,6 @@
 """Database models."""
 
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, String, mapped_column
 
 
 class BaseModel(DeclarativeBase):
@@ -11,3 +11,21 @@ class BaseModel(DeclarativeBase):
     Column types and nullability are derived from the type annotations:
     https://docs.sqlalchemy.org/en/20/orm/declarative_tables.html#mapped-column-derives-the-datatype-and-nullability-from-the-mapped-annotation
     """
+
+
+class Constituent(BaseModel):
+    """An elected official's constituent."""
+
+    __tablename__ = "constituent"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    first_name: Mapped[str] = mapped_column()
+    last_name: Mapped[str] = mapped_column()
+    email: Mapped[str] = mapped_column(unique=True)
+    phone_number: Mapped[str] = mapped_column(String(12))
+    street_address_1: Mapped[str] = mapped_column()
+    street_address_2: Mapped[str] = mapped_column(nullable=True)
+    city: Mapped[str] = mapped_column()
+    state: Mapped[str] = mapped_column(String(2))
+    zip_code: Mapped[str] = mapped_column(String(5))
